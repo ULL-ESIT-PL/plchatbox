7636e14c4fd600e145a2fb84bc70592d5cb0092d
MuhaCP <alu0101434025@ull.edu.es>
2025-07-07 19:52:28 +0100

    correccion route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Message } from "ai";
-import { handleQuery } from "@/lib/handle-query";
+import { Message, OpenAIStream, StreamingTextResponse } from "ai";
+import { handleQuery, handleQueryWithOpenAI } from "@/lib/handle-query";
 
 const formatMessage = (message: Message) => {
     return `${message.role === "user" ? "Human" : "Assistant"}: ${
@@ -11,7 +11,9 @@ const formatMessage = (message: Message) => {
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const messages: Message[] = body.messages ?? [];
+    const topic = body.topic ?? "general";
     console.log("Messages ", messages);
+    console.log("topic", topic);
     const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
     const question = messages[messages.length - 1].content;
   
@@ -31,8 +33,10 @@ export async function POST(req: NextRequest) {
       });
       */
 
-      const answer = await handleQuery(question);
+      const answer = await handleQuery(question, topic);
       return new Response(answer);
+
+
     } catch (error) {
       console.error("Internal server error ", error);
       return NextResponse.json("Error: Something went wrong. Try again!", {
